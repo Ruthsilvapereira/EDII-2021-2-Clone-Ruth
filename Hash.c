2a914b2b9bc8a8a544e2ab2c16d30cc9e451f59c
Ruth Silva <ruthsilva1994@gmail.com>
2021-06-10 13:36:52 +0000

    testt

--- Hash.c
+++ Hash.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Hash.h"
+
 void initHash(HashStruct *hashStruct) {
     for (int i=0;i<MAX;i++) {
         //chamando init de DoublyLinkedList para inicializar cada lista do vetor
@@ -9,9 +10,11 @@ void initHash(HashStruct *hashStruct) {
     }
     hashStruct->size = 0;
 }
+
 bool isHashEmpty(HashStruct *hashStruct) {
     return hashStruct->size==0;
 }
+
 int hash(char *key) {
     int sum = 0;
     // percorremos todos os caracteres da string passada
@@ -21,16 +24,19 @@ int hash(char *key) {
     }
     return sum%MAX; //retorna o resto da divisão
 }
+
 int put(HashStruct *hashStruct, char *key, void *data, compare equal)  {
-    if (!containsKey(hashStruct, key, equal) {
+    if(!containsKey(hashStruct, key, equal)){
         //adiciona na fila que está na posição devolvida pela função hash
         int res = enqueue(&hashStruct->hashes[hash(key)],data);
         //incrementa a qtde de elementos baseado na quantidade inserida por enqueue
         hashStruct->size+=res;
         return res;
     }
+
     return 0;
 }
+
 bool containsKey(HashStruct *hashStruct, char *key, compare equal) {
     //calcula a posição
     int hashValue = hash(key);
@@ -39,6 +45,7 @@ bool containsKey(HashStruct *hashStruct, char *key, compare equal) {
     
     return (pos!=-1)?true:false;
 }
+
 void* get(HashStruct *hashStruct, char *key, compare equal) {
     // descobre em qual fila/lista está o dado
     int hashValue = hash(key);
@@ -47,8 +54,10 @@ void* get(HashStruct *hashStruct, char *key, compare equal) {
     // procuramos o dado na lista
     while(aux!=hashStruct->hashes[hashValue].first && !equal(aux->data, key))
         aux=aux->next;
+
     return aux->data;
 }
+
 void* removeKey(HashStruct *hashStruct, char *key, compare equal) {
     int hashValue = hash(key);
     int pos = indexOf(&hashStruct->hashes[hashValue], key, equal);
@@ -56,6 +65,7 @@ void* removeKey(HashStruct *hashStruct, char *key, compare equal) {
     if (result!=NULL) hashStruct->size--;
     return result;
 }
+
 void showHashStruct(HashStruct *hashStruct, printNode print) {
     printf("There are %d elements in the Hash\n\n",hashStruct->size);
     for (int i=0; i < MAX; i++) {
