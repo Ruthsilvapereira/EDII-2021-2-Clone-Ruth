c46933d2b9ccad5b21659326e04397ead6790bf4
Ruth Silva <ruthsilva1994@gmail.com>
2021-06-19 17:36:07 +0000

    feat atualização

--- Hash.c
+++ Hash.c
@@ -1,7 +1,21 @@
+//Olá a todos! Estou adiantando a atividade Hash do AVA
+//Por gentileza coloque o nome a frente da sua escolha de implementação, comente sempre seu nome em tudo que foi feito e detalhe para melhor endendimento dos demais.
+
+//       5.2.1 initHash ======> Ruth
+//       5.2.2 hash
+//       5.2.3 containsKey
+//       5.2.4 put
+//       5.2.5 get
+//       5.2.6 removeKey
+//       5.2.7 showHashStruct
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "Hash.h"
+
+//Inicio
+//Nesta função de inicialização iremos percorrer cada posição de nossa tabela de hash inicializando a lista duplamente ligada de cada posição (Definição em http://www.jppreti.com/2019/07/29/tabela-hash/#Hashh)
 
 void initHash(HashStruct *hashStruct) {
     for (int i=0;i<MAX;i++) {
--- Hash.h
+++ Hash.h
@@ -1,11 +1,24 @@
+//Operações
+//isHashEmpty: verifica se a tabela de hash está vazia;
+//hash: calcula o hash para a chave;
+//put: insere um par (chave,valor);
+//containsKey: verifica se a chave já existe na tabela de hash;
+//get: retorna o valor buscando pela chave;
+//removeKey: remove um par (chave,valor);
+//showHashStruct: exibe os pares armazenados
+
 #ifndef EstruturaDeDados_Hash_h
 #define EstruturaDeDados_Hash_h
+
 #include "DoublyLinkedList.h"
+
 #define MAX 10
+
 typedef struct HashStruct {
     DoublyLinkedList hashes[MAX];
     int size;
 }HashStruct;
+
 void initHash(HashStruct *hashStruct);
 bool isHashEmpty(HashStruct *hashStruct);
 int hash(char *key);
