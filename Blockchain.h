3444ec586ce9544a46e0beea27a393eeaeb18bb6
Ruth Silva <ruthsilva1994@gmail.com>
2021-06-19 19:48:49 +0000

    ruth

--- Blockchain.h
+++ Blockchain.h
@@ -2,9 +2,11 @@
 #define Blockchain_h
 #include <stdlib.h>
 #include <stdio.h>
+
 enum boolean {
     true = 1, false = 0
 };
+
 typedef enum boolean bool;
 typedef struct Block {
     unsigned int index;
@@ -14,10 +16,12 @@ typedef struct Block {
     float data;
     char* hash;
 }Block;
+
 typedef struct Blockchain {
     Block *genesisBlock;
     Block *latestBlock;
 }Blockchain;
+
 void initBlockchain(Blockchain *blockchain);
 char* calculateHash(int index, char* previousHash, unsigned long timestamp, float data);
 Block* generateNextBlock(Blockchain *blockchain, float data);
